31e5d7b67143ce01ef8cc44afbad9f76fed928dc
阿念Toyotomi <3274457741@qq.com>
2025-12-27 21:08:56 +0800

    Delete zygisk/Android.mk

--- zygisk/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := kawakami_fix
-LOCAL_SRC_FILES := AntiDisk.cpp
-LOCAL_LDLIBS := -llog
-LOCAL_CPPFLAGS := -std=c++20 -fno-exceptions -fno-rtti
-include $(BUILD_SHARED_LIBRARY)
